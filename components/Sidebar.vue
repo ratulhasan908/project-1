1e331496483aca49c23aac25d166b42b9da394ba
Ratul Hasan <ratulhasan.nu@gmail.com>
2024-02-15 12:13:19 +0600

    Default layout is working successfully

---
+++ components/Sidebar.vue
@@ -0,0 +1,173 @@
+<script>
+import { useSidebar } from '../composables/useSidebar'
+
+export default {
+  setup() {
+    const { isOpen } = useSidebar()
+
+    const activeClass = ref(
+      'bg-gray-600 bg-opacity-25 text-gray-100 border-gray-100'
+    )
+    const inactiveClass = ref(
+      'border-gray-900 text-gray-500 hover:bg-gray-600 hover:bg-opacity-25 hover:text-gray-100'
+    )
+
+    return {
+      activeClass,
+      inactiveClass,
+      isOpen
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="flex">
+    <div :class="isOpen ? 'block' : 'hidden'" class="fixed inset-0 z-20 transition-opacity bg-black opacity-50 lg:hidden"
+      @click="isOpen = false" />
+    <div :class="isOpen ? 'translate-x-0 ease-out' : '-translate-x-full ease-in'"
+      class="fixed inset-y-0 left-0 z-30 w-64 overflow-y-auto transition duration-300 transform bg-gray-900 lg:translate-x-0 lg:static lg:inset-0">
+      <div class="brand-area">
+        <h3 class="brand-logo text-center py-3 font-bold text-white"><a href="#">ProAffiliates</a></h3>
+      </div>
+      <div class="flex items-center justify-center mt-4">
+        <div class="flex flex-col items-center">
+          <img class="w-[30%] rounded-full mb-4" src="../assets/img/mc-favicon.png" alt="Monsterclaw fabicon">
+          <span class="mx-2 text-md font-semibold text-white">MonsterClaw LTD</span>
+        </div>
+      </div>
+      <nav class="mt-10">
+        <router-link class="flex items-center px-6 py-2 mt-4 duration-200 border-l-4"
+          :class="[$route.name === 'Getting_Started' ? activeClass : inactiveClass]" @click="isOpen = false"
+          to="/getting_started">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" height="1em" viewBox="0 0 512 512">
+            <path
+              d="M156.6 384.9L125.7 354c-8.5-8.5-11.5-20.8-7.7-32.2c3-8.9 7-20.5 11.8-33.8L24 288c-8.6 0-16.6-4.6-20.9-12.1s-4.2-16.7 .2-24.1l52.5-88.5c13-21.9 36.5-35.3 61.9-35.3l82.3 0c2.4-4 4.8-7.7 7.2-11.3C289.1-4.1 411.1-8.1 483.9 5.3c11.6 2.1 20.6 11.2 22.8 22.8c13.4 72.9 9.3 194.8-111.4 276.7c-3.5 2.4-7.3 4.8-11.3 7.2v82.3c0 25.4-13.4 49-35.3 61.9l-88.5 52.5c-7.4 4.4-16.6 4.5-24.1 .2s-12.1-12.2-12.1-20.9V380.8c-14.1 4.9-26.4 8.9-35.7 11.9c-11.2 3.6-23.4 .5-31.8-7.8zM384 168a40 40 0 1 0 0-80 40 40 0 1 0 0 80z" />
+          </svg>
+          <span class="mx-4">Getting Started</span>
+        </router-link>
+
+        <router-link class="flex items-center px-6 py-2 mt-4 duration-200 border-l-4"
+          :class="[$route.name === 'dashboard' ? activeClass : inactiveClass]" @click="isOpen = false" to="/dashboard">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" height="1em" viewBox="0 0 576 512">
+            <path
+              d="M543.8 287.6c17 0 32-14 32-32.1c1-9-3-17-11-24L512 185V64c0-17.7-14.3-32-32-32H448c-17.7 0-32 14.3-32 32v36.7L309.5 7c-6-5-14-7-21-7s-15 1-22 8L10 231.5c-7 7-10 15-10 24c0 18 14 32.1 32 32.1h32v69.7c-.1 .9-.1 1.8-.1 2.8V472c0 22.1 17.9 40 40 40h16c1.2 0 2.4-.1 3.6-.2c1.5 .1 3 .2 4.5 .2H160h24c22.1 0 40-17.9 40-40V448 384c0-17.7 14.3-32 32-32h64c17.7 0 32 14.3 32 32v64 24c0 22.1 17.9 40 40 40h24 32.5c1.4 0 2.8 0 4.2-.1c1.1 .1 2.2 .1 3.3 .1h16c22.1 0 40-17.9 40-40V455.8c.3-2.6 .5-5.3 .5-8.1l-.7-160.2h32z" />
+          </svg>
+          <span class="mx-4">Dashboard</span>
+        </router-link>
+        <router-link class="flex items-center px-6 py-2 mt-4 duration-200 border-l-4"
+          :class="[$route.name === 'Campaigns' ? activeClass : inactiveClass]" @click="isOpen = false" to="/campaigns">
+          <svg class="w-5 h-5" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink" width="64px" height="64px" viewBox="0 0 38 32"
+            enable-background="new 0 0 38 32" xml:space="preserve" fill="currentColor">
+            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+            <g id="SVGRepo_iconCarrier">
+              <g>
+                <path fill="currentColor"
+                  d="M6.502,22H7h1v-0.004c0,0.002,0.274,0.002,0.3,0.001c2.018-0.11,12.446-0.315,18.67,5.649 c0.016,0.016,0.032,0.028,0.048,0.044c0.112,0.108,0.233,0.203,0.342,0.315c0.002,0.002,0.004,0.002,0.006,0.004 c0.983,0.882,2.082,1.386,3.255,1.386c4.201,0,7.37-6.319,7.37-14.698C37.991,6.319,34.822,0,30.621,0 c-1.138,0-2.207,0.473-3.168,1.305C19.028,8.569,8.526,8.997,8.437,9H7H6.472C2.782,9,0,11.648,0,15.161v0.196 C0,18.958,2.978,22,6.502,22z M36.991,14.698c0,7.553-2.857,13.698-6.37,13.698c-3.577,0-6.599-6.273-6.599-13.698 C24.022,7.273,27.044,1,30.621,1C34.133,1,36.991,7.145,36.991,14.698z M8.453,10c0.097,0,8.733-0.348,16.772-5.745 c-1.368,2.645-2.203,6.32-2.203,10.443c0,4.006,0.793,7.581,2.093,10.207C18.691,20.73,10.315,20.899,8.431,21H7.369 c-0.373-1.291-1.546-6-0.006-11H8.453z M1,15.161c0-2.894,2.281-5.076,5.324-5.147c-1.402,4.784-0.456,9.308,0.001,10.977 C3.421,20.89,1,18.354,1,15.356V15.161z">
+                </path>
+                <path fill="currentColor"
+                  d="M25.5,11H25c-0.276,0-0.5,0.224-0.5,0.5S24.724,12,25,12h0.5c1.425,0,2.5,1.032,2.5,2.429 C28,15.847,26.878,17,25.5,17H25c-0.276,0-0.5,0.224-0.5,0.5S24.724,18,25,18h0.5c1.93,0,3.5-1.602,3.5-3.599 C29,12.494,27.462,11,25.5,11z">
+                </path>
+                <path fill="currentColor"
+                  d="M8.5,23.5C8.224,23.5,8,23.724,8,24v3.951C8,30.184,9.794,32,12,32s4-1.816,4-4.049V24 c0-0.276-0.224-0.5-0.5-0.5S15,23.724,15,24v3.951C15,29.632,13.654,31,12,31s-3-1.368-3-3.049V24C9,23.724,8.776,23.5,8.5,23.5z">
+                </path>
+              </g>
+            </g>
+          </svg>
+          <span class="mx-4">Campaigns</span>
+        </router-link>
+        <router-link class="flex items-center px-6 py-2 mt-4 duration-200 border-l-4"
+          :class="[$route.name === 'Promoters' ? activeClass : inactiveClass]" @click="isOpen = false" to="/promoters">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" height="1em" viewBox="0 0 640 512">
+            <path
+              d="M96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3zM609.3 512H471.4c5.4-9.4 8.6-20.3 8.6-32v-8c0-60.7-27.1-115.2-69.8-151.8c2.4-.1 4.7-.2 7.1-.2h61.4C567.8 320 640 392.2 640 481.3c0 17-13.8 30.7-30.7 30.7zM432 256c-31 0-59-12.6-79.3-32.9C372.4 196.5 384 163.6 384 128c0-26.8-6.6-52.1-18.3-74.3C384.3 40.1 407.2 32 432 32c61.9 0 112 50.1 112 112s-50.1 112-112 112z" />
+          </svg>
+          <span class="mx-4">Promoters</span>
+        </router-link>
+        <router-link class="flex items-center px-6 py-2 mt-4 duration-200 border-l-4"
+          :class="[$route.name === 'Promotions' ? activeClass : inactiveClass]" @click="isOpen = false" to="/promotions">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" height="1em" viewBox="0 0 512 512">
+            <path
+              d="M16.1 260.2c-22.6 12.9-20.5 47.3 3.6 57.3L160 376V479.3c0 18.1 14.6 32.7 32.7 32.7c9.7 0 18.9-4.3 25.1-11.8l62-74.3 123.9 51.6c18.9 7.9 40.8-4.5 43.9-24.7l64-416c1.9-12.1-3.4-24.3-13.5-31.2s-23.3-7.5-34-1.4l-448 256zm52.1 25.5L409.7 90.6 190.1 336l1.2 1L68.2 285.7zM403.3 425.4L236.7 355.9 450.8 116.6 403.3 425.4z" />
+          </svg>
+          <span class="mx-4">Promotions</span>
+        </router-link>
+        <router-link class="flex items-center px-6 py-2 mt-4 duration-200 border-l-4" @click="isOpen = false"
+          :class="[$route.name === 'Referred Leads' ? activeClass : inactiveClass]" to="/referred-leads">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" height="1em" viewBox="0 0 512 512">
+            <path
+              d="M384 48c8.8 0 16 7.2 16 16V448c0 8.8-7.2 16-16 16H96c-8.8 0-16-7.2-16-16V64c0-8.8 7.2-16 16-16H384zM96 0C60.7 0 32 28.7 32 64V448c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64H96zM240 256a64 64 0 1 0 0-128 64 64 0 1 0 0 128zm-32 32c-44.2 0-80 35.8-80 80c0 8.8 7.2 16 16 16H336c8.8 0 16-7.2 16-16c0-44.2-35.8-80-80-80H208zM512 80c0-8.8-7.2-16-16-16s-16 7.2-16 16v64c0 8.8 7.2 16 16 16s16-7.2 16-16V80zM496 192c-8.8 0-16 7.2-16 16v64c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm16 144c0-8.8-7.2-16-16-16s-16 7.2-16 16v64c0 8.8 7.2 16 16 16s16-7.2 16-16V336z" />
+          </svg>
+          <span class="mx-4">Referred Leads</span>
+        </router-link>
+        <router-link class="flex items-center px-6 py-2 mt-4 duration-200 border-l-4"
+          :class="[$route.name === 'Referred Customers' ? activeClass : inactiveClass]" @click="isOpen = false"
+          to="/referred-customers">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" height="1em" viewBox="0 0 576 512">
+            <path
+              d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96zM252 160c0 11 9 20 20 20h44v44c0 11 9 20 20 20s20-9 20-20V180h44c11 0 20-9 20-20s-9-20-20-20H356V96c0-11-9-20-20-20s-20 9-20 20v44H272c-11 0-20 9-20 20z" />
+          </svg>
+          <span class="mx-4">Customers</span>
+        </router-link>
+        <router-link class="flex items-center px-6 py-2 mt-4 duration-200 border-l-4"
+          :class="[$route.name === 'Rewards' ? activeClass : inactiveClass]" @click="isOpen = false" to="/rewards">
+          <svg class="w-5 h-5" width="64px" height="64px" viewBox="0 0 24 24" fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+            <g id="SVGRepo_iconCarrier">
+              <circle cx="12" cy="12" r="10" stroke="currentColor" stroke-width="1.5"></circle>
+              <path d="M12 6V18" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"></path>
+              <path
+                d="M15 9.5C15 8.11929 13.6569 7 12 7C10.3431 7 9 8.11929 9 9.5C9 10.8807 10.3431 12 12 12C13.6569 12 15 13.1193 15 14.5C15 15.8807 13.6569 17 12 17C10.3431 17 9 15.8807 9 14.5"
+                stroke="currentColor" stroke-width="1.5" stroke-linecap="round"></path>
+            </g>
+          </svg>
+          <span class="ml-4">Rewards/Commissions</span>
+        </router-link>
+        <router-link class="flex items-center px-6 py-2 mt-4 duration-200 border-l-4"
+          :class="[$route.name === 'Payouts' ? activeClass : inactiveClass]" @click="isOpen = false" to="/payouts">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" height="1em" viewBox="0 0 576 512">
+            <path
+              d="M312 24V34.5c6.4 1.2 12.6 2.7 18.2 4.2c12.8 3.4 20.4 16.6 17 29.4s-16.6 20.4-29.4 17c-10.9-2.9-21.1-4.9-30.2-5c-7.3-.1-14.7 1.7-19.4 4.4c-2.1 1.3-3.1 2.4-3.5 3c-.3 .5-.7 1.2-.7 2.8c0 .3 0 .5 0 .6c.2 .2 .9 1.2 3.3 2.6c5.8 3.5 14.4 6.2 27.4 10.1l.9 .3c11.1 3.3 25.9 7.8 37.9 15.3c13.7 8.6 26.1 22.9 26.4 44.9c.3 22.5-11.4 38.9-26.7 48.5c-6.7 4.1-13.9 7-21.3 8.8V232c0 13.3-10.7 24-24 24s-24-10.7-24-24V220.6c-9.5-2.3-18.2-5.3-25.6-7.8c-2.1-.7-4.1-1.4-6-2c-12.6-4.2-19.4-17.8-15.2-30.4s17.8-19.4 30.4-15.2c2.6 .9 5 1.7 7.3 2.5c13.6 4.6 23.4 7.9 33.9 8.3c8 .3 15.1-1.6 19.2-4.1c1.9-1.2 2.8-2.2 3.2-2.9c.4-.6 .9-1.8 .8-4.1l0-.2c0-1 0-2.1-4-4.6c-5.7-3.6-14.3-6.4-27.1-10.3l-1.9-.6c-10.8-3.2-25-7.5-36.4-14.4c-13.5-8.1-26.5-22-26.6-44.1c-.1-22.9 12.9-38.6 27.7-47.4c6.4-3.8 13.3-6.4 20.2-8.2V24c0-13.3 10.7-24 24-24s24 10.7 24 24zM568.2 336.3c13.1 17.8 9.3 42.8-8.5 55.9L433.1 485.5c-23.4 17.2-51.6 26.5-80.7 26.5H192 32c-17.7 0-32-14.3-32-32V416c0-17.7 14.3-32 32-32H68.8l44.9-36c22.7-18.2 50.9-28 80-28H272h16 64c17.7 0 32 14.3 32 32s-14.3 32-32 32H288 272c-8.8 0-16 7.2-16 16s7.2 16 16 16H392.6l119.7-88.2c17.8-13.1 42.8-9.3 55.9 8.5zM193.6 384l0 0-.9 0c.3 0 .6 0 .9 0z" />
+          </svg>
+          <span class="mx-4">Payouts</span>
+        </router-link>
+        <router-link class="flex items-center px-6 py-2 mt-4 duration-200 border-l-4"
+          :class="[$route.name === 'Reports' ? activeClass : inactiveClass]" @click="isOpen = false" to="/reports">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" height="1em" viewBox="0 0 448 512">
+            <path
+              d="M160 80c0-26.5 21.5-48 48-48h32c26.5 0 48 21.5 48 48V432c0 26.5-21.5 48-48 48H208c-26.5 0-48-21.5-48-48V80zM0 272c0-26.5 21.5-48 48-48H80c26.5 0 48 21.5 48 48V432c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V272zM368 96h32c26.5 0 48 21.5 48 48V432c0 26.5-21.5 48-48 48H368c-26.5 0-48-21.5-48-48V144c0-26.5 21.5-48 48-48z" />
+          </svg>
+          <span class="mx-4">Reports</span>
+        </router-link>
+        <router-link class="flex items-center px-6 py-2 mt-4 duration-200 border-l-4"
+          :class="[$route.name === 'Assets' ? activeClass : inactiveClass]" @click="isOpen = false" to="/assets">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" height="1em" viewBox="0 0 576 512">
+            <path
+              d="M88.7 223.8L0 375.8V96C0 60.7 28.7 32 64 32H181.5c17 0 33.3 6.7 45.3 18.7l26.5 26.5c12 12 28.3 18.7 45.3 18.7H416c35.3 0 64 28.7 64 64v32H144c-22.8 0-43.8 12.1-55.3 31.8zm27.6 16.1C122.1 230 132.6 224 144 224H544c11.5 0 22 6.1 27.7 16.1s5.7 22.2-.1 32.1l-112 192C453.9 474 443.4 480 432 480H32c-11.5 0-22-6.1-27.7-16.1s-5.7-22.2 .1-32.1l112-192z" />
+          </svg>
+          <span class="mx-4">Assets</span>
+        </router-link>
+        <router-link class="flex items-center px-6 py-2 mt-4 duration-200 border-l-4"
+          :class="[$route.name === 'Emails' ? activeClass : inactiveClass]" @click="isOpen = false" to="/emails">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" height="1em" viewBox="0 0 512 512">
+            <path
+              d="M64 112c-8.8 0-16 7.2-16 16v22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1V128c0-8.8-7.2-16-16-16H64zM48 212.2V384c0 8.8 7.2 16 16 16H448c8.8 0 16-7.2 16-16V212.2L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64H448c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128z" />
+          </svg>
+          <span class="mx-4">Emails</span>
+        </router-link>
+        <router-link class="flex items-center px-6 py-2 mt-4 duration-200 border-l-4"
+          :class="[$route.name === 'Team' ? activeClass : inactiveClass]" @click="isOpen = false" to="/team">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" height="1em" viewBox="0 0 640 512">
+            <path
+              d="M144 0a80 80 0 1 1 0 160A80 80 0 1 1 144 0zM512 0a80 80 0 1 1 0 160A80 80 0 1 1 512 0zM0 298.7C0 239.8 47.8 192 106.7 192h42.7c15.9 0 31 3.5 44.6 9.7c-1.3 7.2-1.9 14.7-1.9 22.3c0 38.2 16.8 72.5 43.3 96c-.2 0-.4 0-.7 0H21.3C9.6 320 0 310.4 0 298.7zM405.3 320c-.2 0-.4 0-.7 0c26.6-23.5 43.3-57.8 43.3-96c0-7.6-.7-15-1.9-22.3c13.6-6.3 28.7-9.7 44.6-9.7h42.7C592.2 192 640 239.8 640 298.7c0 11.8-9.6 21.3-21.3 21.3H405.3zM224 224a96 96 0 1 1 192 0 96 96 0 1 1 -192 0zM128 485.3C128 411.7 187.7 352 261.3 352H378.7C452.3 352 512 411.7 512 485.3c0 14.7-11.9 26.7-26.7 26.7H154.7c-14.7 0-26.7-11.9-26.7-26.7z" />
+          </svg>
+          <span class="mx-4">Team</span>
+        </router-link>
+      </nav>
+    </div>
+  </div>
+</template>
