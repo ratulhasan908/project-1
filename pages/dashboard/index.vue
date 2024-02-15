a153d75f99491952b12ec5ee3c421c01b15526fc
Ratul Hasan <ratulhasan.nu@gmail.com>
2024-02-15 12:49:54 +0600

    Data fetched successfully

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,12 +1,32 @@
+<script>
+export default {
+    data() {
+        return {
+            photos: [],
+        }
+    },
+    mounted() {
+        this.getPhotos();
+    },
+    methods: {
+        async getPhotos() {
+            try {
+                const response = await fetch("https://jsonplaceholder.typicode.com/photos")
+                console.log(response);
+                this.photos = await response.json();
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    },
+}
+</script>
+
 <template>
-    <div>
-        <h2>This is dashboard of my new layout</h2>
-        <h2 class="bg-black text-white my-3 py-5 text-center">Yes its working</h2>
+    <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-8">
+        <div v-for="photo in photos" :key="photo.i">
+            <img :src="photo.url" alt="Photo">
+            <p>{{photo.title}}</p>
+        </div>
     </div>
 </template>
-
-<script>
-    export default {
-        
-    }
-</script>
