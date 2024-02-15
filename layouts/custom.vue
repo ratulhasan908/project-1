ae538f72d17b77558c865b37137a4a18bd45e1a0
Ratul Hasan <ratulhasan.nu@gmail.com>
2024-02-15 12:29:06 +0600

    Custom layout is working successfully

---
+++ layouts/custom.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="flex h-screen font-roboto dashboard-body">
+        <div class="flex-1 flex flex-col overflow-hidden">
+            <main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
+                <div class="main-body-layout mx-0 px-6 py-8">
+                    <slot />
+                </div>
+            </main>
+        </div>
+        <Sidebar />
+
+    </div>
+</template>
--- pages/about.vue
+++ pages/about.vue
@@ -1,12 +1,12 @@
+<script lang="ts">
+definePageMeta({
+  layout: 'custom'
+})
+</script>
+
 <template>
     <div>
         <TestView />
         <h1>Hello this is from about page, its an experiment</h1>
     </div>
 </template>
-
-<script>
-    export default {
-        
-    }
-</script>
