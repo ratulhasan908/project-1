7661adf372ca2cd58dcf938ed25fdf9f9581b30c
Ratul Hasan <ratulhasan.nu@gmail.com>
2024-02-17 09:19:39 -0800

    Changed the sidebar item

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,17 +1,14 @@
 <script>
 import { useSidebar } from '../composables/useSidebar'
-
 export default {
   setup() {
     const { isOpen } = useSidebar()
-
     const activeClass = ref(
       'bg-gray-600 bg-opacity-25 text-gray-100 border-gray-100'
     )
     const inactiveClass = ref(
       'border-gray-900 text-gray-500 hover:bg-gray-600 hover:bg-opacity-25 hover:text-gray-100'
     )
-
     return {
       activeClass,
       inactiveClass,
