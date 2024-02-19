379144439239265237d18f38b726f8063bc9be7c
Ratul Hasan <ratulhasan.nu@gmail.com>
2024-02-19 15:25:32 +0600

    Script tag has been modified, dashboard

--- pages/index.vue
+++ pages/index.vue
@@ -1,51 +1,3 @@
-<template>
-    <div class="w-full max-w-md px-2 py-16 sm:px-0">
-        <TabGroup>
-            <TabList class="flex space-x-1 rounded-xl bg-blue-900/20 p-1">
-                <Tab v-for="category in Object.keys(categories)" as="template" :key="category" v-slot="{ selected }">
-                    <button :class="[
-                        'w-full rounded-lg py-2.5 text-sm font-medium leading-5',
-                        'ring-white/60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
-                        selected
-                            ? 'bg-white text-blue-700 shadow'
-                            : 'text-blue-100 hover:bg-white/[0.12] hover:text-white',
-                    ]">
-                        {{ category }}
-                    </button>
-                </Tab>
-            </TabList>
-
-            <TabPanels class="mt-2">
-                <TabPanel v-for="(posts, idx) in Object.values(categories)" :key="idx" :class="[
-                    'rounded-xl bg-white p-3',
-                    'ring-white/60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
-                ]">
-                    <ul>
-                        <li v-for="post in posts" :key="post.id" class="relative rounded-md p-3 hover:bg-gray-100">
-                            <h3 class="text-sm font-medium leading-5">
-                                {{ post.title }}
-                            </h3>
-
-                            <ul class="mt-1 flex space-x-1 text-xs font-normal leading-4 text-gray-500">
-                                <li>{{ post.date }}</li>
-                                <li>&middot;</li>
-                                <li>{{ post.commentCount }} comments</li>
-                                <li>&middot;</li>
-                                <li>{{ post.shareCount }} shares</li>
-                            </ul>
-
-                            <a href="#" :class="[
-                                'absolute inset-0 rounded-md',
-                                'ring-blue-400 focus:z-10 focus:outline-none focus:ring-2',
-                            ]" />
-                        </li>
-                    </ul>
-                </TabPanel>
-            </TabPanels>
-        </TabGroup>
-    </div>
-</template>
-  
 <script setup>
 import { ref } from 'vue'
 import { TabGroup, TabList, Tab, TabPanels, TabPanel } from '@headlessui/vue'
@@ -101,4 +53,52 @@ const categories = ref({
     ],
 })
 </script>
+
+<template>
+    <div class="w-full max-w-md px-2 py-16 sm:px-0">
+        <TabGroup>
+            <TabList class="flex space-x-1 rounded-xl bg-blue-900/20 p-1">
+                <Tab v-for="category in Object.keys(categories)" as="template" :key="category" v-slot="{ selected }">
+                    <button :class="[
+                        'w-full rounded-lg py-2.5 text-sm font-medium leading-5',
+                        'ring-white/60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
+                        selected
+                            ? 'bg-white text-blue-700 shadow'
+                            : 'text-blue-100 hover:bg-white/[0.12] hover:text-white',
+                    ]">
+                        {{ category }}
+                    </button>
+                </Tab>
+            </TabList>
+
+            <TabPanels class="mt-2">
+                <TabPanel v-for="(posts, idx) in Object.values(categories)" :key="idx" :class="[
+                    'rounded-xl bg-white p-3',
+                    'ring-white/60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
+                ]">
+                    <ul>
+                        <li v-for="post in posts" :key="post.id" class="relative rounded-md p-3 hover:bg-gray-100">
+                            <h3 class="text-sm font-medium leading-5">
+                                {{ post.title }}
+                            </h3>
+
+                            <ul class="mt-1 flex space-x-1 text-xs font-normal leading-4 text-gray-500">
+                                <li>{{ post.date }}</li>
+                                <li>&middot;</li>
+                                <li>{{ post.commentCount }} comments</li>
+                                <li>&middot;</li>
+                                <li>{{ post.shareCount }} shares</li>
+                            </ul>
+
+                            <a href="#" :class="[
+                                'absolute inset-0 rounded-md',
+                                'ring-blue-400 focus:z-10 focus:outline-none focus:ring-2',
+                            ]" />
+                        </li>
+                    </ul>
+                </TabPanel>
+            </TabPanels>
+        </TabGroup>
+    </div>
+</template>
   
